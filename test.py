056d97d914f7fe5eec974b095be92ce471063d89
Alber Analafean <105194751+alberanalafean22@users.noreply.github.com>
2024-12-19 01:52:48 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-
-# Kode Streamlit
-
 import streamlit as st
 import git
+import subprocess
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 
 # Repository GitHub
 repo_url = "https://github.com/alberanalafean22/convnextkeras"
@@ -13,8 +13,8 @@ model_name = "convnextaugmentasiepochs50.keras"
 # Fungsi load model
 def load_model(repo_url, model_name):
     repo = git.Repo.clone_from(repo_url, "temp_repo")
-    git-lfs install
-    git-lfs pull
+    subprocess.run(["git", "lfs", "install"], cwd="temp_repo")
+    subprocess.run(["git", "lfs", "pull"], cwd="temp_repo")
     return "temp_repo/" + model_name
 
 # Load model
@@ -23,3 +23,4 @@ model_path = load_model(repo_url, model_name)
 # Tampilkan model
 st.write("Model berhasil di-load dari GitHub!")
 st.write(model_path)
+
